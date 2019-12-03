dc3423b496d60af7525b9f1775a6f116de69b1ff
Bharat Uday <balupillai88@gmail.com>
2019-12-04 01:47:19 +0530

    Updated implementation of binarySearchTree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,156 +1,101 @@
-class Node {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
+// Space & Time Complexity is O(log(n)) and O(1) respectively.
+
+class BinarySearchTree {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  class BinarySearchTree {
-    constructor() {
-      this.root = null;
-    }
-  
-    insert(value) {
-      function insertHelper(node, value) {
-        if (node === null) {
-          this.root = new Node(value);
-          return this.root;
+  insert(value){
+    let currentNode = this;
+    while(true){
+      if(value < currentNode.value){
+        if(currentNode.left === null){
+          currentNode.left = new BinarySearchTree(value);
+          break;
+        }else{
+          currentNode = currentNode.left;
         }
-        if (value <= node.value) {
-          if (node.left === null) {
-            node.left = new Node(value);
-            return node.left;
-          } else {
-            insertHelper(node.left, value);
-          }
-        } else {
-          if (node.right === null) {
-            node.right = new Node(value);
-            return node.right;
-          } else {
-            insertHelper(node.right, value);
-          }
+      }else{
+        if(currentNode.right === null){
+          currentNode.right = new BinarySearchTree(value);
+          break;
+        }else{
+          currentNode = currentNode.right;
         }
       }
-      return insertHelper.call(this, this.root, value);
     }
-  
-    contains(value) {
-      function containsHelper(node, value) {
-        if (node === null) {
-          return false;
-        }
-        if (node.value === value) {
-          return true;
-        } else if (value < node.value && node.left !== null) {
-          return containsHelper(node.left, value);
-        } else if (value > node.value && node.left !== null) {
-          return containsHelper(node.right, value);
-        }
-        return false;
-      }
-  
-      return containsHelper.call(this, this.root, value);
-    }
-  
-    min(node = null) {
-      if (node === null) {
-        return;
-      }
-      if (node.left === null) {
-        return node;
-      } else {
-        return this.min(node.left);
-      }
-    }
-  
-    max(node) {
-      if (node === null) {
-        return null;
-      }
-  
-      if (node.right === null) {
-        return node;
-      } else {
-        return this.max(node.right);
-      }
-    }
-  
-    remove(value) {
-      function removeHelper(node, value) {
-        if (node.value === value) {
-          // No children, remove the current node
-          if (node.left === null && node.right === null) {
-            node = null;
-            return node;
-          }
-          // One child
-          if (node.left === null) {
-            node = node.right;
-            return node;
-          }
-          if (node.right === null) {
-            node = node.left;
-            return node;
-          }
-          // two children
-          let successor = this.min(node.right);
-          let temp = node.value;
-          node.value = successor.value;
-          successor.value = temp;
-          node.right = removeHelper(node.right, temp);
-          return node;
-        } else if (value < node.value && node.left !== null) {
-          node.left = removeHelper.call(this, node.left, value);
-        } else if (value > node.value && node.right !== null) {
-          node.right = removeHelper.call(this, node.right, value);
-        }
-        return node;
-      }
-      this.root = removeHelper.call(this, this.root, value);
-    }
-  
-    // left, root, right
-    inOrderTraversal(node, func = console.log) {
-      if (node === null) {
-        return false;
-      }
-      if (node.left !== null) {
-        this.inOrderTraversal(node.left, func);
-      }
-      func(node);
-      if (node.right !== null) {
-        this.inOrderTraversal(node.right, func);
-      }
-    }
-  
-    // root, left, right
-    preOrderTraversal(node, func = console.log) {
-      if (node === null) {
-        return false;
-      }
-      func(node);
-      if (node.left !== null) {
-        this.preOrderTraversal(node.left, func);
-      }
-      if (node.right !== null) {
-        this.preOrderTraversal(node.right, func);
-      }
-    }
-  
-    // left, right, root
-    postOrderTraversal(node, func = console.log) {
-      if (node === null) {
-        return false;
-      }
-      if (node.left !== null) {
-        this.postOrderTraversal(node.left, func);
-      }
-      if (node.right !== null) {
-        this.postOrderTraversal(node.right, func);
-      }
-      func(node);
-    }
+    return this;
   }
-  
+  contains(value){
+    let currentNode = this;
+    while(currentNode !== null){
+      if(value < currentNode.value){
+        currentNode = currentNode.left;
+      }else if(value > currentNode.value){
+        currentNode = currentNode.right;
+      }else{
+        return true;
+      }
+    }
+    return false;
+  }
+  remove(value,parentNode = null){
+    let currentNode = this;
+    while(currentNode !== null){
+      if(value < currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      }else if (value > currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      }else{
+        if(currentNode.left !== null && currentNode.right !== null){
+          currenNode.value = currentNode.right.getMinValue();
+          currentNode.right.remove(currentNode.value,currentNode);
+        }else if(parentNode === null){ //root Node
+          if(currentNode.left !== null){
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          }else if(currentNode.right !== null){
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          }else{
+            currentNode.value = null;
+          }
+        }else if(parentNode.left === currentNode){
+          parentNode.left = currentNode.left !== null?currentNode.left:currentNode.right;
+        }else if(parentNode.right === currentNode){
+          parentNode.right = currentNode.right !== null ? currentNode.left : currentNode.right;
+        }
+        break;
+      }
+    }
+    return this;
+  }
+  getMinValue(){
+    let currentNode = this;
+    while(currentNode.left !== null){
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+  getMaxValue(){
+    let currentNode = this;
+    while(currentNode.right !== null){
+      currentNode = currentNode.right;
+    }
+    return currentNode.value;
+  }
+}
+
+const treeOne = new BinarySearchTree(10);
+treeOne.insert(5);
+treeOne.insert(4);
+treeOne.insert(6);
+treeOne.insert(12);
+console.log("tree is: "+treeOne);
+console.log("Min node is: ",treeOne.getMinValue());
+console.log("Max node is: ",treeOne.getMaxValue());
